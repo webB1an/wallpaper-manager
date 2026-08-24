f9c57c9d226c914bc46ab67e47058cdf5c5395b5
yoohoyoo <475008394@qq.com>
2026-08-24 17:05:26 +0800

    Trigger restart to load wechat secret

--- apps/miniprogram/utils/ads.js
+++ apps/miniprogram/utils/ads.js
@@ -4,6 +4,7 @@ exports.AD_UNITS = void 0;
 // 流量主广告位 ID，替换成微信流量主后台生成的单元 ID。
 // 留空时页面不会渲染广告组件，不会影响功能。
 exports.AD_UNITS = {
+    // 广告位 ID 由流量主后台创建；留空表示不展示。
     homeBanner: "adunit-6e47ccafdacca7d5",
     listBanner: "adunit-d816603ff2347e3f",
     detailBanner: "adunit-7e8102dfa72fa78b",
